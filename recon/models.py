84be9f07c657ddbc7ef84a37bd71e64be1167957
amit <charayaamit@gmail.com>
2021-11-23 12:58:33 +0530

    database added

--- recon/models.py
+++ recon/models.py
@@ -5,12 +5,23 @@ class Account(models.Model):
     number=models.CharField(max_length=16)
     name=models.CharField(max_length=250)
     openingBalance=models.FloatField()
+    date = models.DateField()
+    def __str__(self):
+        return self.name
 
 class Transactions(models.Model):
+    txnNo=models.CharField(max_length=25)
     #account=
+    date:models.DateField()
     drTxn=models.FloatField()
     crTxn=models.FloatField()
     #txnTpe=
 
+    def __str__(self):
+        return self.txnNo
+
 class TxnType(models.Model):
     type=models.CharField(max_length=26)
+
+    def __str__(self):
+        return self.type
--- recon/views.py
+++ recon/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render
-
+from .models import Account
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    accounts=Account.objects.all()
+    context={'accounts':accounts}
+    return render(request,'home.html',context)
