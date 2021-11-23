30a76dd38573eee94da55237a692d62a43fa9dde
amit <charayaamit@gmail.com>
2021-11-23 11:20:21 +0530

    database added

--- recon/models.py
+++ recon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    number=models.CharField(max_length=16)
+    name=models.CharField(max_length=250)
+    openingBalance=models.FloatField()
+
+class Transactions(models.Model):
+    #account=
+    drTxn=models.FloatField()
+    crTxn=models.FloatField()
+    #txnTpe=
+
+class TxnType(models.Model):
+    type=models.CharField(max_length=26)
